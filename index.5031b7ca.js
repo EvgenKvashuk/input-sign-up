159727180b75ee5ed83da6017f3c49483dc4d6ed
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-05-06 12:20:00 +0000

    Deploying to gh-pages from @ EvgenKvashuk/input-sign-up@2ee1cdeffdbfe58cd31069c3931477804945ae11 🚀

---
+++ index.5031b7ca.js
@@ -0,0 +1,2 @@
+let e=0;const t=document.querySelector(".countBtn"),c=document.querySelector(".acum");t.addEventListener("click",(t=>{e++,c.innerHTML=e}));
+//# sourceMappingURL=index.5031b7ca.js.map
---
+++ index.979978b9.js
@@ -0,0 +1,2 @@
+!function(){var e=0,n=document.querySelector(".countBtn"),c=document.querySelector(".acum");n.addEventListener("click",(function(n){e++,c.innerHTML=e}))}();
+//# sourceMappingURL=index.979978b9.js.map
