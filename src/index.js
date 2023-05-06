db10c3d9a280026eb6b0eec88b3ff7fd556fa11d
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-05-06 10:05:28 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
+// modal
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+// scrol
 window.onload = () => {
     window.onscroll = function (e) {
         let winy = window.scrolly;
-        if (winy > 300) {
-            progressbar();
+        // if (winy > 1) {
+        //     // progressbar();
 
-            scrollbaranimation();
+        //     // scrollbaranimation();
 
-            winy = null;
-        }
+        //     // winy = null;
+        // }
     };
 
     const scrolbtn = document.querySelector('.isshowbtn');
     window.onscroll = () => {
-        if (window.scrollY > 300) {
+        if (window.scrollY > 1) {
             scrolbtn.classList.remove('isshowbtn__hide')
-        } else if (window.scrollY < 300) {
+        } else if (window.scrollY < 1) {
             scrolbtn.classList.add('isshowbtn__hide')
         }
     };
 
     scrolbtn.onclick = () => {
         window.scrollTo(0, 0);
-
     };
 
 }
