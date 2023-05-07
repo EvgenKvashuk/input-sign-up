2ef2accfb1b254bb9bd62d1cef732978ada0e9bf
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-05-07 16:23:58 +0000

    Deploying to gh-pages from @ EvgenKvashuk/input-sign-up@65b27e71a0467bccf6e8815a994a7e5b00e7bf51 🚀

---
+++ index.bf2ada78.js
@@ -0,0 +1,2 @@
+let e=0;const o=document.querySelector(".countBtn"),c=document.querySelector("#hide"),n=document.querySelector(".acum");o.addEventListener("click",(o=>{e++,e>0&&(c.classList.remove("is-hiden"),console.log("спрацювало")),console.log(e),n.innerHTML=e}));
+//# sourceMappingURL=index.bf2ada78.js.map
