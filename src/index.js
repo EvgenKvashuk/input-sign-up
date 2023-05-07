65b27e71a0467bccf6e8815a994a7e5b00e7bf51
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-05-07 19:23:34 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 // modal
 
-(() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open-sign]"),
-        closeModalBtn: document.querySelector("[data-modal-close-sign]"),
-        modal: document.querySelector("[data-modal-sign]"),
-    };
+// (() => {
+//     const refs = {
+//         openModalBtn: document.querySelector("[data-modal-open-sign]"),
+//         closeModalBtn: document.querySelector("[data-modal-close-sign]"),
+//         modal: document.querySelector("[data-modal-sign]"),
+//     };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
 
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-    }
-})();
+//     function toggleModal() {
+//         refs.modal.classList.toggle("is-hidden");
+//     }
+// })();
 
 // scrol
-window.onload = () => {
-    const scrolbtn = document.querySelector('.isshowbtn');
+// window.onload = () => {
+//     const scrolbtn = document.querySelector('.isshowbtn');
 
-    window.onscroll = () => {
-        if (window.scrollY > 500) {
-            scrolbtn.classList.remove('isshowbtn__hide')
-        } else if (window.scrollY < 500) {
-            scrolbtn.classList.add('isshowbtn__hide')
-        }
-    };
+//     window.onscroll = () => {
+//         if (window.scrollY > 500) {
+//             scrolbtn.classList.remove('isshowbtn__hide')
+//         } else if (window.scrollY < 500) {
+//             scrolbtn.classList.add('isshowbtn__hide')
+//         }
+//     };
 
-    scrolbtn.onclick = (evt) => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    };
-}
+//     scrolbtn.onclick = (evt) => {
+//         window.scrollTo({
+//             top: 0,
+//             behavior: "smooth"
+//         });
+//     };
+// }
 
 
 
@@ -42,13 +42,24 @@ window.onload = () => {
 // counter
 let x = 0;
 const counter = document.querySelector('.countBtn');
+const shopList = document.querySelector('#hide');
+
 const acum = document.querySelector('.acum');
 
 function count() {
 	x++;
+
+    if (x > 0) {
+        shopList.classList.remove('is-hiden')
+        console.log('спрацювало')
+    }
+
+    console.log(x)
+
 	acum.innerHTML = x;
 }
 
 counter.addEventListener('click', (evt) => {
 count();
+
 })
