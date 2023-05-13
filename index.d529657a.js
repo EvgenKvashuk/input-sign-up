af87e6f8fab2c1ddad3696d3fd1844d05071a22c
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-05-13 08:06:41 +0000

    Deploying to gh-pages from @ EvgenKvashuk/input-sign-up@930bd1d13a49fc42df1cdfaf8056cdb96f8a99fa 🚀

--- index.d529657a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=0,n=document.querySelector(".countBtn"),o=document.querySelector("#hide"),c=document.querySelector(".acum");n.addEventListener("click",(function(n){++e>0&&(o.classList.remove("is-hiden"),console.log("спрацювало")),console.log(e),c.innerHTML=e}))}();
-//# sourceMappingURL=index.d529657a.js.map
