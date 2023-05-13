930bd1d13a49fc42df1cdfaf8056cdb96f8a99fa
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-05-13 11:06:06 +0300

    add

--- src/index.js
+++ src/index.js
@@ -40,26 +40,79 @@
 
 
 // counter
-let x = 0;
-const counter = document.querySelector('.countBtn');
-const shopList = document.querySelector('#hide');
+// let x = 0;
+// const counter = document.querySelector('.countBtn');
+// const shopList = document.querySelector('#hide');
 
-const acum = document.querySelector('.acum');
+// const acum = document.querySelector('.acum');
 
-function count() {
-	x++;
+// function count() {
+// 	x++;
 
-    if (x > 0) {
-        shopList.classList.remove('is-hiden')
-        console.log('спрацювало')
-    }
+//     if (x > 0) {
+//         shopList.classList.remove('is-hiden')
+//         console.log('спрацювало')
+//     }
 
-    console.log(x)
+//     console.log(x)
 
-	acum.innerHTML = x;
+// 	acum.innerHTML = x;
+// }
+
+// counter.addEventListener('click', (evt) => {
+// count();
+
+// })
+
+
+
+
+
+
+
+
+
+// при вході на сайт визивається функція яка перевіряє чи є щось в локал сторедж
+// перевіряє значення {key: dark} і додає класси відповідно за результатами перевірки
+
+const LOCAL_STORAGE_KEY = 'theme';
+
+const refs = {
+    btnChange: document.querySelector('.theme-switcher-input'),
+
+    hBook: document.querySelector('.book-title'),
+    aBook: document.querySelector('.author-name'),
+    dBook: document.querySelector('.modal-book-description'),
+
+    primary: document.querySelector('.sign-up-btn'),
+    aBtn: document.querySelector('.current'),
+    bgc: document.querySelector('body'),
 }
 
-counter.addEventListener('click', (evt) => {
-count();
+const curentTheme = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
 
-})
+function changetheme() {
+    refs.btnChange.classList.toggle(".dark")
+    refs.hBook.classList.toggle(".dark")
+    refs.aBook.classList.toggle(".dark")
+
+    // refs.dBook.classList.toggle(".dark")
+    // refs.aBtn.classList.toggle(".dark")
+}
+
+// if (curentTheme = 'dark') {
+//     changetheme()
+// }
+
+// Декларуємо колбек для евента натискання на кнопку
+const handleClick = () => {
+    console.log("click event listener callback");
+    // перевіряємо чи у чекбокса є класс темноє теми, так - передаємо в локал сторедж
+    // записуємо обьєкт в локал сторедж з значенням дарк {key: dark}
+    // перевіряємо чи є в локал сторедж темна тема, якщо є то додаємо класи на елементи розмітки, якщо ні то прибираємо
+    changetheme()
+
+};
+
+// евент та визов колбека
+refs.btnChange.addEventListener("click", handleClick);
